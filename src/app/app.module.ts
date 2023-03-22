15b0984fccd92cac39d8f2c1f06a469ed8f1c635
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-22 17:33:19 +0200

    later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';  
 import {EmployeeComponent} from './component/employee/employee.component'
 
@@ -9,9 +10,10 @@ import {EmployeeComponent} from './component/employee/employee.component'
     EmployeeComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,EmployeeComponent]
 })
 export class AppModule { }
