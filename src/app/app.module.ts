bb88ba44d91b734b326b20d597179d4a99d1e4e4
Fausio <LUISFAUSIO@GMAIL.COM>
2023-03-26 18:20:47 +0200

    updatesbootdey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './component/employee/employee.component';
-import { HttpClientModule } from '@angular/common/http'
-import { EmployeeService } from './service/employee.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [EmployeeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
